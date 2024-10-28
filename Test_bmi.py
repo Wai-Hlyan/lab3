bebd077c40ce50776096a2282681fa5866b6e55b
WaiHlyan <waihlyan.23@ichat.sp.edu.sg>
2024-10-28 09:32:19 +0800

    added gitignore file and testfile successfully

---
+++ Test_bmi.py
@@ -0,0 +1,10 @@
+from Lab2.bmi import calculate_bmi as bmi  # type: ignore
+
+def test_underweight(): 
+    assert bmi(1.75, 50) == -1
+
+def test_normal_weight():
+    assert bmi(1.75, 68) == 0
+
+def test_overweight():
+    assert bmi(1.75, 80) == 1   
